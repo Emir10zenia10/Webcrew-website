590cb354b62c7c3ae751631dde39a3c5bdae3844
Emir Ecom <emir.ecom.business@gmail.com>
2026-09-20 22:33:23 +0200

    WebCrew V3: remove global scroll atmosphere from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,10 @@ import { Hero } from "@/components/Hero";
 import { PerceptionGap } from "@/components/PerceptionGap";
 import { HomeSections } from "@/components/HomeSections";
 import { SiteFooter } from "@/components/SiteFooter";
-import { ScrollAtmosphere } from "@/components/ScrollAtmosphere";
 
 export default function Page() {
   return (
     <>
-      <ScrollAtmosphere />
       <Header />
       <main id="main-content">
         <Hero />
